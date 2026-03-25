185ad38d6dbb8dcedb239f5cc5e9ed0f9d03aa79
Isanka <isanka@sunstonels.com>
2026-03-25 12:30:35 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         react(),
         keycloakify({
             themeName: ["vanila", "chocolate"],
-            accountThemeImplementation: "none"
+            accountThemeImplementation: "Single-Page"
         })
     ]
 });
